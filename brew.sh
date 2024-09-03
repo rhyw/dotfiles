68e725beb45eb8710e7393b0a9b10a05d5c71ad4
Yuguang Wang <yuwang@redhat.com>
2024-09-03 18:41:41 +0800

    brew.sh: remove redundant module installations

--- brew.sh
+++ brew.sh
@@ -14,13 +14,7 @@ brew install findutils
 # GNU `sed`
 brew install gnu-sed
 
-
-# Updated shells
-# Note: don’t forget to add `/usr/local/bin/<EACHSHELL>` to `/etc/shells` before running `chsh`.
-brew install fish
-
 brew install bash-completion
-
 
 # Install wget
 brew install wget
@@ -29,6 +23,7 @@ brew install wget
 brew install vim
 brew install nano
 brew install grep
+brew install rsync
 
 # z hopping around folders
 brew install z
@@ -46,28 +41,13 @@ brew install glances
 
 brew install shellcheck # linting for .sh files
 
-
-# mtr - ping & traceroute. best.
-brew install mtr
-   # `abbr mtr "sudo mtr"` to avoid forgetting.
-
-# Install other useful binaries
-brew install the_silver_searcher # ack is an alternative, tbh i forget which i like more.
 brew install fzf
 
-brew install imagemagick
 brew install node # This installs `npm` too using the recommended installation method
-brew install rename
 brew install tree
-brew install ffmpeg
 
 # json stuff
 brew install jq gron
 
-# brew install ncdu # find where your diskspace went
-brew install gdu # available as gdu-go. faster than ncdu on SSD's.
-
-
-brew install scrcpy # control/view android phone from PC. amazing
-brew install youtube-dl
+brew install yt-dlp
 
