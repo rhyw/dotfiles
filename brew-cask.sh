89bb4bdfb5480e0df92166acd2f723c88aa92c19
Yuguang Wang <yuwang@redhat.com>
2024-09-03 15:03:23 +0800

    brew-cask: get rid of less favorable packages

--- brew-cask.sh
+++ brew-cask.sh
@@ -12,18 +12,4 @@ brew install --cask rectangle
 brew install --cask 1password
 brew install --cask rescuetime
 
-# dev
-brew install --cask iterm2
-brew install --cask sublime-text
-brew install --cask imageoptim
-
-
-# less often
-brew install --cask disk-inventory-x
-brew install --cask screenflow
-brew install --cask vlc
-
 brew install --cask spotify
-
-brew tap homebrew/cask-fonts
-brew install --cask font-fira-code
